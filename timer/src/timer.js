4e96f96e4fce1a8017e9d31d00021ef0ef6201e9
Malkey <lintucrow@yandex.ru>
2024-12-09 23:12:52 +0300

    add remove btn

--- timer/src/timer.js
+++ timer/src/timer.js
@@ -1,17 +1,31 @@
-export default (container, time) => {
+const createTimer = (container, time) => {
   const li = document.createElement('li');
+  const span = document.createElement('span');
+  const btn = document.createElement('button');
 
-  li.innerHTML = `Осталось ${time} секунд`;
+  span.innerHTML = `Осталось ${time} секунд`;
+  btn.innerHTML = '&times;';
+
+  li.append(span, btn);
   container.append(li);
 
   const timer = setInterval(() => {
     if (time > 0) {
       time -= 1;
-      li.innerHTML = `Осталось ${time} секунд`;
+      span.innerHTML = `Осталось ${time} секунд`;
     } else {
-      clearInterval(timer);
-      li.remove();
+      removeTimer();
     }
   }, 1000);
-  
-}
+
+  const removeTimer = () => {
+    clearInterval(timer);
+    li.remove();
+  };
+
+  btn.addEventListener('click', () => {
+    removeTimer();
+  });
+};
+
+export default createTimer;
