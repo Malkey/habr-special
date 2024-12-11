afe9cdbc2e2a2187a56076e54d878c38387eec65
Malkey <lintucrow@yandex.ru>
2024-12-11 19:29:11 +0300

    add sorting

--- reviews/src/components/App/App.jsx
+++ reviews/src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import Table from '../Table/Table'
 import Filters from '../Filters/Filters'
+import Sorting from '../Sorting/Sorting'
 import './App.css'
 
 function App() {
   return (
     <>
       <Filters />
+      <Sorting />
       <Table />
     </>
   )
---
+++ reviews/src/components/Sorting/Sorting.jsx
@@ -0,0 +1,44 @@
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux'
+import { sortData } from '../../redux/actions';
+
+const Filters = () => {
+    const dispatch = useDispatch();
+    
+    const handleChange = useCallback((e) => {
+        dispatch(sortData({
+            by: e.target.name,
+            order: e.target.value,
+        }));
+    }, [dispatch]);
+
+    return (
+        <div style={{ textAlign: 'left', marginBottom: '30px' }}>
+            <div>
+                <p>Сортировать по дате:</p>
+                <label>
+                    Сначала новые
+                    <input type="radio" name="date" value="desc" onChange={handleChange} />
+                </label>
+                <label>
+                    Сначала старые
+                    <input type="radio" name="date" value="asc" onChange={handleChange} />
+                </label>
+            </div>
+
+            <div>
+                <p>Сортировать по оценке:</p>
+                <label>
+                    По убыванию
+                    <input type="radio" name="rating" value="desc" onChange={handleChange} />
+                </label>
+                <label>
+                    По возрастанию
+                    <input type="radio" name="rating" value="asc" onChange={handleChange} />
+                </label>
+            </div>
+        </div>
+    );
+};
+
+export default Filters;
