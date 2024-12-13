cbd088799f8e2cdb43cb41212810e299df5d010a
Malkey <lintucrow@yandex.ru>
2024-12-13 20:30:55 +0300

    add data generation

--- reviews/src/redux/data.js
+++ reviews/src/redux/data.js
@@ -1,23 +1,44 @@
-export default [
-    {
-        id: 1,
-        platform: "Google",
-        rating: 4,
-        date: "2023-11-15T10:00:00Z",
-        text: "Отличный сервис!",
-    },
-    {
-        id: 2,
-        platform: "Яндекс",
-        rating: 3,
-        date: "2023-11-14T09:00:00Z",
-        text: "Хорошо, но есть недочеты.",
-    },
-    {
-        id: 3,
-        platform: "2ГИС",
-        rating: 5,
-        date: "2023-11-13T08:00:00Z",
-        text: "Прекрасно!",
-    },
-];
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * (max + 1));
+    // 0..max
+};
+
+const getRandomDate = () => {
+    const start = new Date('2020-01-01').getTime();
+    const end = Date.now();
+    const randomDate = start + Math.random() * (end - start);
+    return new Date(randomDate);
+};
+
+const getRandomPlatform = () => {
+    const platforms = ['Google', 'Яндекс', '2ГИС'];
+    const randomIndex = getRandomInt(2);
+    return platforms[randomIndex];
+};
+
+const getReviewText = (rating) => {
+    const texts = {
+        1: 'Невозможно пользоваться!',
+        2: 'Есть над чем поработать.',
+        3: 'Хорошо, но есть недочеты.',
+        4: 'Очень хорошо!',
+        5: 'Прекрасно! Отличный сервис!',
+    };
+    return texts[rating];
+};
+
+const getReview = () => {
+    const id = self.crypto.randomUUID();
+    const platform = getRandomPlatform();
+    const rating = getRandomInt(4) + 1;
+    const date = getRandomDate();
+    const text = getReviewText(rating);
+
+    return { id, platform, rating, date, text };
+};
+
+const generateReviews = (num) => [...Array(num)].map(() => getReview());
+
+const reviews = generateReviews(100);
+
+export default reviews;
