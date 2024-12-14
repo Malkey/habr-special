131c930919e4ab4357af48df199a2494bb12789b
Malkey <lintucrow@yandex.ru>
2024-12-14 22:42:57 +0300

    add column component

--- kanban/src/app/components/Board/Board.jsx
+++ kanban/src/app/components/Board/Board.jsx
@@ -1,112 +1,14 @@
-import { useCallback, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { addCard, changeStatus } from './../../slices/cardsSlice';
-import { useDrop } from "react-dnd";
-import Card from './../Card/Card';
+import Column from "../Column/Column";
 import styles from './board.module.css';
 
-const getId = () => {
-    return new Date().getTime();
-}
-
-const Board = () => {
-    const dispatch = useDispatch();
-    const cards = useSelector(state => state.cards);
-    const input = useRef();
-
-    const [, dropInProgress] = useDrop({
-        accept: 'CARD',
-        drop: (item) => {
-            dispatch(changeStatus({
-                id: item.id,
-                newStatus: 'in-progress',
-            }));
-        },
-    });
-
-    const [, dropUnderReview] = useDrop({
-        accept: 'CARD',
-        drop: (item) => {
-            dispatch(changeStatus({
-                id: item.id,
-                newStatus: 'under-review',
-            }));
-        },
-    });
-
-    const [, dropCompleted] = useDrop({
-        accept: 'CARD',
-        drop: (item) => {
-            dispatch(changeStatus({
-                id: item.id,
-                newStatus: 'completed',
-            }));
-        },
-    });
-
-    const handleFormSubmit = useCallback((e) => {
-        e.preventDefault();
-        const value = input.current.value.trim();
-
-        if (value !== '') {
-            dispatch(addCard({
-                id: getId(),
-                title: value,
-                status: "in-progress"
-            }));
-        }
-
-        e.target.reset();
-    }, [dispatch]);
-    
+const Board = () => {   
     return (
         <>
             <h1>Канбан-доска</h1>
-
             <div className={styles.board}>
-                <div ref={dropInProgress} className={styles.column}>
-                    <h2 className={styles.h2}>В работе</h2>
-                    {cards.map(card => (
-                        card.status === 'in-progress' && 
-                        <Card
-                            key={card.id}
-                            id={card.id}
-                            title={card.title}
-                        />
-                    ))}
-                    <form onSubmit={handleFormSubmit}>
-                        <input 
-                            type="text"
-                            className={styles.input}
-                            placeholder="Добавить задачу"
-                            ref={input}
-                        />
-                    </form>
-                </div>
-
-                <div ref={dropUnderReview} className={styles.column}>
-                    <h2 className={styles.h2}>На проверке</h2>
-                    {cards.map(card => (
-                        card.status === 'under-review' && 
-                        <Card
-                            key={card.id}
-                            id={card.id}
-                            title={card.title}
-                        />
-                    ))}
-                </div>
-
-                <div ref={dropCompleted} className={styles.column}>
-                    <h2 className={styles.h2}>Завершено</h2>
-                    {cards.map(card => (
-                        card.status === 'completed' && 
-                        <Card
-                            key={card.id}
-                            id={card.id}
-                            title={card.title}
-                        />
-                    ))}
-                </div>
+                <Column name="in-progress" />
+                <Column name="under-review" />
+                <Column name="completed" />
             </div>
         </>
     );
---
+++ kanban/src/app/components/Column/Column.js
@@ -0,0 +1,75 @@
+import { useCallback, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addCard, changeStatus } from '../../slices/cardsSlice';
+import { useDrop } from 'react-dnd';
+import Card from "../Card/Card";
+import styles from './column.module.css';
+
+const getId = () => {
+    return new Date().getTime();
+}
+
+const Column = ({ name }) => {
+    const cards = useSelector(state => state.cards);
+    const dispatch = useDispatch();
+    const input = useRef();
+
+    const title = {
+        'in-progress': 'В работе',
+        'under-review': 'На проверке',
+        'completed': 'Завершено',
+    }
+    
+    const [, dropRef] = useDrop({
+        accept: 'CARD',
+        drop: (item) => {
+            dispatch(changeStatus({
+                id: item.id,
+                newStatus: name,
+            }));
+        },
+    });
+
+    const handleFormSubmit = useCallback((e) => {
+        e.preventDefault();
+        const value = input.current.value.trim();
+
+        if (value !== '') {
+            dispatch(addCard({
+                id: getId(),
+                title: value,
+                status: "in-progress"
+            }));
+        }
+
+        e.target.reset();
+    }, [dispatch]);
+
+    return (
+        <div ref={dropRef} className={styles.column}>
+            <h2 className={styles.h2}>{title[name]}</h2>
+
+            {cards.map(card => (
+                card.status === name && 
+                <Card
+                    key={card.id}
+                    id={card.id}
+                    title={card.title}
+                />
+            ))}
+
+            {name === 'in-progress' &&
+                <form onSubmit={handleFormSubmit}>
+                    <input 
+                        type="text"
+                        className={styles.input}
+                        placeholder="Добавить задачу"
+                        ref={input}
+                    />
+                </form>
+            }
+        </div>
+    );
+}
+
+export default Column;
