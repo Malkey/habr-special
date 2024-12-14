15268c34e23050bc8a3e4babc6d9a8f6a5182e3e
Malkey <lintucrow@yandex.ru>
2024-12-14 21:23:01 +0300

    add empty data check

--- reviews/src/components/Table/Table.jsx
+++ reviews/src/components/Table/Table.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { getData } from '../../redux/actions';
 import './Table.css';
@@ -19,25 +19,32 @@ const Table = () => {
             <td>{item.text}</td>
         </tr>
     ));
+
+    const isDataEmpty = useCallback(
+        () => visibleData.length === 0,
+        [visibleData]
+    );
     
     return (
         loading
             ?   <p>Данные загружаются</p>
             :   error
                 ?   <p>При загрузке данных произошла ошибка: {error}</p>
-                :   <table>
-                        <thead>
-                            <tr>
-                                <th>Платформа</th>
-                                <th>Рейтинг</th>
-                                <th>Время добавления</th>
-                                <th>Текст отзыва</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {rows}
-                        </tbody>
-                    </table>
+                :   isDataEmpty() 
+                    ?   <p>Ничего не найдено, попробуйте выбрать другие фильтры.</p>
+                    :   <table>
+                            <thead>
+                                <tr>
+                                    <th>Платформа</th>
+                                    <th>Рейтинг</th>
+                                    <th>Время добавления</th>
+                                    <th>Текст отзыва</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {rows}
+                            </tbody>
+                        </table>
     );
 };
 
